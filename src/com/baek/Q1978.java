e6f941f473b40adadd02394febbd0ed50ac9f34f
coder013 <wnssh607@gmail.com>
2021-09-07 00:44:35 +0900

    소수 찾기 문제 풀이

--- src/com/baek/Q1978.java
+++ src/com/baek/Q1978.java
@@ -5,39 +5,39 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Q1978 {
-	public static void main(String[] args) {
-		try {
-			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-			int count = 0;
-			int size = Integer.parseInt(br.readLine());
-			int[] numbers = new int[size];
+            int count = 0;
+            int size = Integer.parseInt(br.readLine());
+            int[] numbers = new int[size];
 
-			StringTokenizer st = new StringTokenizer(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
 
-			for (int i = 0; i < size; i++) {
-				numbers[i] = Integer.parseInt(st.nextToken());
-			}
+            for (int i = 0; i < size; i++) {
+                numbers[i] = Integer.parseInt(st.nextToken());
+            }
 
-			for (int number : numbers) {
-				if (number == 2) {
-					continue;
-				} else if (number == 1 || number % 2 == 0) {
-					count++;
-					continue;
-				}
+            for (int number : numbers) {
+                if (number == 2) {
+                    continue;
+                } else if (number == 1 || number % 2 == 0) {
+                    count++;
+                    continue;
+                }
 
-				for (int i = 3; i * i <= number; i = i + 2) {
-					if (number % i == 0) {
-						count++;
-						break;
-					}
-				}
-			}
+                for (int i = 3; i * i <= number; i = i + 2) {
+                    if (number % i == 0) {
+                        count++;
+                        break;
+                    }
+                }
+            }
 
-			System.out.println(size - count);
-		} catch (Exception e) {
-			System.out.println(e);
-		}
-	}
+            System.out.println(size - count);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
 }
